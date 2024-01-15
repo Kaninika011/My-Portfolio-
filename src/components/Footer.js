bc707f738b23963d6ba32f9a6859a2a67450abee
Kaninika Sen <kaninikasc@gmail.com>
2024-01-15 19:40:08 +0530

    initial commit

---
+++ src/components/Footer.js
@@ -0,0 +1,31 @@
+import { Container, Row, Col } from "react-bootstrap";
+import { MailchimpForm } from "./MailchimpForm";
+import logo from '../assets/images/logo.png';
+import navIcon1 from '../assets/images/nav-icon1.svg';
+import navIcon2 from '../assets/images/nav-icon2.svg';
+ 
+
+const  Footer = () => {
+   return (
+        <footer className="footer">
+           <Container>
+           
+            <Row className="align-item-center">
+            
+               <MailchimpForm/>
+                    
+                    <Col size={12} sm={6}>
+  
+                    <div className="social-icon ">
+              <a href="https://www.linkedin.com/in/kaninika-sen-choudhuri-2a6b101b7/"><img src={navIcon1} alt="Icon" /></a>
+              <a href="https://github.com/Kaninika011"><img src={navIcon2} alt="Icon" /></a>
+              </div>
+              <p>Copyright 2024. All Rights Reserved</p>
+              </Col>
+           
+            </Row>
+            </Container> 
+        </footer>
+    )
+ }
+ export default Footer; 
